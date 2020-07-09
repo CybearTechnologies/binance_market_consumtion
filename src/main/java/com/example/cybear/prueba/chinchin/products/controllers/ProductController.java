151b873e6eda6dfa9a5718bbdbd634af4e75e6ff
José Cedeño <jafjdev@gmail.com>
2020-07-09 15:47:52 -0400

    [refactor] change ProductController class name.

--- src/main/java/com/example/cybear/prueba/chinchin/products/controllers/ProductController.java
+++ src/main/java/com/example/cybear/prueba/chinchin/products/controllers/ProductController.java
@@ -3,5 +3,5 @@ package com.example.cybear.prueba.chinchin.products.controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ProductControllers {
+public class ProductController {
 }
