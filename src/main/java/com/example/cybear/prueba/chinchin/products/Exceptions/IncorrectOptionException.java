670cb15f02893568a63c4e191cb87df3b772ad48
José Cedeño <jafjdev@gmail.com>
2020-07-09 20:10:28 -0400

    [feat] Se agregaron las nuevas opciones de bss y petros.

--- src/main/java/com/example/cybear/prueba/chinchin/products/Exceptions/IncorrectOptionException.java
+++ src/main/java/com/example/cybear/prueba/chinchin/products/Exceptions/IncorrectOptionException.java
@@ -2,6 +2,6 @@ package com.example.cybear.prueba.chinchin.products.Exceptions;
 
 public class IncorrectOptionException extends CustomException {
     public IncorrectOptionException() {
-        super("Por favor, seleccionar una opcion valida, BTCUSDT,DASHUSDT,EURUSDT,ETHUSDT");
+        super("Por favor, seleccionar una opcion valida, BTCUSDT,DASHUSDT,EURUSDT,ETHUSDT,BSSUSDT,PTRUSDT");
     }
 }
